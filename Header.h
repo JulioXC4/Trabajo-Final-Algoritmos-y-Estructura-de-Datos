6ac10bfcf66522318431953a7b1265113fab0d6e
JulioXC4 <danielc4nt@gmail.com>
2024-04-22 20:20:25 -0500

    prueba de comit

---
+++ Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+
+#include <iostream>
+
+void printMessage() {
+    std::cout << "¡Hola desde el header file!" << std::endl;
+}
+
+#endif // EXAMPLE_H
